6a635d5bc3b130fcd56540f31b439d9297222131
bhattanzil06 <tanzilkhalil190@icloud.com>
2023-11-27 12:27:26 +0100

    Atrribues Player

---
+++ src/Bottle.java
@@ -0,0 +1,7 @@
+import greenfoot.*;
+
+import java.util.List;
+
+public class Bottle extends Actor{
+
+}
--- src/Player.java
+++ src/Player.java
@@ -7,6 +7,52 @@ import java.util.List;
  * @version 1.0
  */
 public class Player extends Actor {
+    //Attribute
+
+    private int life = 100;
+    private int level = 1;
+    private int stamina = 25;
+    private int coins = 0;
+    private int speed = 1;
 
 
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 }

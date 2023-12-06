d67f88595eb518ef12dc51314aa21902ebdd3510
bhattanzil06 <tanzilkhalil190@icloud.com>
2023-12-06 11:58:05 +0100

    Moving-und ImprovedActor importiert performMovement

---
+++ src/Character.java
@@ -0,0 +1,60 @@
+public class Character extends MovingActor {
+    //Attribute
+
+    private int life = 100;
+    private int level = 1;
+    private int stamina = 25;
+    private int coins = 0;
+    private int speed = 1;
+    private int experience = 0;
+
+    //Getter & Setter
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -6,64 +6,19 @@ import java.util.List;
  * @author SAE
  * @version 1.0
  */
-public class Player extends MovingActor {
+public class Player extends Character {
     //Attribute
-
-    private int life = 100;
-    private int level = 1;
-    private int stamina = 25;
-    private int coins = 0;
-    private int speed = 1;
-    private int experience = 0;
 
     //Getter & Setter
 
-    public int getLife() {
-        return life;
-    }
-
-    public void setLife(int life) {
-        this.life = life;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public int getStamina() {
-        return stamina;
-    }
-
-    public void setStamina(int stamina) {
-        this.stamina = stamina;
-    }
-
-    public int getCoins() {
-        return coins;
-    }
-
-    public void setCoins(int coins) {
-        this.coins = coins;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        this.experience = experience;
+    public Player(){
+        setLevel(1);
+        setLife(100);
+        setStamina(100);
+        setCoins(0);
+        setExperience(0);
+        setSpeed(1);
+        setExperience(0);
     }
 
     //Methoden
@@ -72,7 +27,7 @@ public class Player extends MovingActor {
         if (Greenfoot.isKeyDown("W")) {
             turn(Direction.NORTH);
             if (!canMove()) {
-                setLife(this.life - 10);
+                setLife(getLife() - 10);
             } else {
                 move();
             }
@@ -80,7 +35,7 @@ public class Player extends MovingActor {
         if (Greenfoot.isKeyDown("A")) {
             turn(Direction.WEST);
             if (!canMove()) {
-                setLife(this.life - 10);
+                setLife(getLife() - 10);
             } else {
                 move();
             }
@@ -88,7 +43,7 @@ public class Player extends MovingActor {
         if (Greenfoot.isKeyDown("S")) {
             turn(Direction.SOUTH);
             if (!canMove()) {
-                setLife(this.life - 10);
+                setLife(getLife() - 10);
             } else {
                 move();
             }
@@ -97,7 +52,7 @@ public class Player extends MovingActor {
         if (Greenfoot.isKeyDown("D")) {
             turn(Direction.EAST);
             if (!canMove()) {
-                setLife(this.life - 10);
+                setLife(getLife() - 10);
             } else {
                 move();
             }
@@ -105,9 +60,9 @@ public class Player extends MovingActor {
     }
 
     public void move() {
-        if (stamina > 0) {
+        if (getStamina() > 0) {
             move(1);
-            setStamina(this.stamina - 10);
+            setStamina(getStamina() - 10);
         }
 
     }

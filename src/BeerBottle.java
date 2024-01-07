0631937ef69a6060a6b54c7d310217938588c452
MilkaKuh <151740831+MilkaKuh@users.noreply.github.com>
2024-01-07 22:33:27 +0100

    Delete src directory

--- src/BeerBottle.java
+++
@@ -1,6 +0,0 @@
-public class BeerBottle extends Bottle{
-
-    public BeerBottle(){
-        setWorth(3);
-    }
-}
--- src/Homeless.java
+++
@@ -1,6 +0,0 @@
-public class Homeless extends Mob{
-    public void act(){
-        monsterPerformMovement();
-        monsterAttack(30,20);
-    }
-}
